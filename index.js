262668a8d59936c3eff73d8d5873843423456b50
Serhii Mykhalskyi <serhii.mykhalskyi@gmail.com>
2024-06-15 18:51:34 +0300

    Delay removed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+
 const categories = require('./data/categories.json');
 const products = require('./data/products.json');
 
@@ -8,17 +9,13 @@ const app = express();
 app.use(cors());
 
 app.get('/categories', (req, res) => {
-  setTimeout(() => {
-    res.json(categories);
-  }, 2000);
+  res.json(categories);
 });
 
 app.get('/products', (req, res) => {
   const shuffleProducts = [...products].sort(() => Math.random() - 0.5);
 
-  setTimeout(() => {
-    res.json(shuffleProducts);
-  }, 2000);
+  res.json(shuffleProducts);
 });
 
 app.get('/:products/:id', (req, res) => {
@@ -28,15 +25,14 @@ app.get('/:products/:id', (req, res) => {
   const product = productFile.find((item) => item.id === productId);
 
   if (!product) {
-    return res.status(404).json({ error: 'Product not found' });
+    return res.status(404).send('Product not found');
   }
 
-  setTimeout(() => {
-    res.json(product);
-  }, 2000);
+  res.json(product);
 });
 
 const port = process.env.PORT || 4000;
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
